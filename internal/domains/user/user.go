d708e8bfe6054c3f68971741bf600c35a13e7cff
The Mj <m.javad1391@gmail.com>
2023-06-13 15:33:06 +0330

    Add User models

--- internal/domains/user/user.go
+++ internal/domains/user/user.go
@@ -1,1 +1,34 @@
 package user
+
+import "context"
+
+// UserRepositoryInterface defines set of methods which every repository who wants to play role as user repo should obey
+type UserRepositoryInterface interface {
+	GetUserById(id int) (*User, error)
+	GetUserByUUID(uuid string) (*User, error)
+	GetUserByEmail(email string) (*User, error)
+	UserExists(email string) bool
+	CreateUser(user *User) (*User, error)
+	UpdateUser(user *User, username, password *string) (*User, error)
+	DeleteUser(user *User) (*User, error)
+}
+
+// UserServiceInterface defines set of methods which every service who wants to play role as user service should obey.
+type UserServiceInterface interface {
+	GetUserById(id int) (*User, error)
+	GetUserByUUID(uuid string) (*User, error)
+	GetUserByEmail(email string) (*User, error)
+	UserExists(email string) bool
+	CreateUser(username *string, email, password string) (*User, error)
+	UpdateUser(userId int, username, password *string) (*User, error)
+	DeleteUser(userId int, password *string) (*User, error)
+}
+
+// UserUseCaseInterface defines set of methods which every use case who wants to play role as user use case should obey.
+type UserUseCaseInterface interface {
+	Register(ctx context.Context, request *UserRegisterRequest) (*AuthResponse, error)
+	Login(ctx context.Context, request *UserLoginRequest) (*AuthResponse, error)
+	UpdateUserPass(ctx context.Context, request *UpdateUserRequest) (*User, error)
+	UpdateUserName(ctx context.Context, request *UpdateUserRequest) (*User, error)
+	DeleteUser(ctx context.Context, request *DeleteUserRequest) (*User, error)
+}
