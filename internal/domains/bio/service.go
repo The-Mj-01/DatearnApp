960f0f4c5bae98aa92ad95ad127fcc809b7ebab0
The Mj <m.javad1391@gmail.com>
2023-06-18 18:01:11 +0330

    Write the base of service & test it

--- internal/domains/bio/service.go
+++ internal/domains/bio/service.go
@@ -1,1 +1,63 @@
 package bio
+
+import "time"
+
+type BioService struct {
+	repo BioRepositoryInterface
+}
+
+func NewService(repo BioRepositoryInterface) BioServiceInterface {
+	return &BioService{
+		repo: repo,
+	}
+}
+
+func (b BioService) GetBioById(id uint) (*Bio, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (b BioService) GetBioByCountry(countryId uint) (*Bio, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (b BioService) GetBioByCity(cityId uint) (*Bio, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (b BioService) GetBioBySex(sexId uint) (*Bio, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (b BioService) GetBioByBorn(bornDate time.Time) (*Bio, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (b BioService) GetBioByBornAfter(bornDate time.Time) (*Bio, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (b BioService) GetBioByCountryCitySex(countryId, cityId, sexId uint) (*Bio, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (b BioService) GetBioByCountryCitySexBornAfterDate(countryId, cityId, sexId uint, bornDate time.Time) (*Bio, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (b BioService) CreateBio(description string, socialMedia, country, city, sex uint, born time.Time) (*Bio, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (b BioService) UpdateBio(description string, socialMedia, country, city, sex uint, born time.Time) (*Bio, error) {
+	//TODO implement me
+	panic("implement me")
+}
--- internal/domains/bio/service_test.go
+++ internal/domains/bio/service_test.go
@@ -1,1 +1,7 @@
 package bio
+
+import "gorm.io/gorm"
+
+func createService(db *gorm.DB) BioServiceInterface {
+	return NewService(NewRepository(db))
+}
