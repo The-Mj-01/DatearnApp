0ce23638bbce06d4e0da4b32d51c5e3ea5db7456
The Mj <m.javad1391@gmail.com>
2023-06-20 16:10:01 +0330

    Write base of Bio UseCase

--- internal/domains/bio/usecase.go
+++ internal/domains/bio/usecase.go
@@ -1,1 +1,30 @@
 package bio
+
+import "context"
+
+// BioUseCase is a struct which satisfies user use case interface functionalities
+type BioUseCase struct {
+	sv BioServiceInterface
+}
+
+// NewUserUseCase and return it
+func NewUserUseCase(sv BioServiceInterface) BioUseCaseInterface {
+	return &BioUseCase{
+		sv: sv,
+	}
+}
+
+func (b BioUseCase) WriteBio(ctx context.Context, token string, request *BioCreateRequest) (*Bio, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (b BioUseCase) GetBio(ctx context.Context, token string, request *BioGetSingleRequest) (*Bio, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (b BioUseCase) UpdateBio(ctx context.Context, token string, request *BioUpdateRequest) (*Bio, error) {
+	//TODO implement me
+	panic("implement me")
+}
