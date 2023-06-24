767d2f8790bab8af629836ad93b0ec5965f197ee
The Mj <the.king.mj99@gmail.com>
2023-06-24 22:39:11 +0330

    write GetAllSocialMedia repository test & implement GetAllSocialMedia repository method

--- internal/domains/socialMedia/repository.go
+++ internal/domains/socialMedia/repository.go
@@ -1,1 +1,35 @@
 package socialMedia
+
+import "gorm.io/gorm"
+
+type SocialMediaRepository struct {
+	db *gorm.DB
+}
+
+func NewSocialMediaRepository(db *gorm.DB) SocialMediaRepositoryInterface {
+	return &SocialMediaRepository{
+		db: db,
+	}
+}
+
+func (s *SocialMediaRepository) GetAllSocialMedia(id *uint, name *string, limit *int, offset int) *[]SocialMedia {
+	var social *[]SocialMedia
+
+	db := s.db
+
+	if name != nil {
+		db = db.Where("name LIKE ?", *name)
+	}
+
+	if limit != nil {
+		db = db.Limit(*limit)
+	}
+
+	db.Offset(offset).Find(&social)
+	return social
+}
+
+func (s *SocialMediaRepository) CreateSocialMedia(name string) (*SocialMedia, error) {
+	//TODO implement me
+	panic("implement me")
+}
