68d44107e9a92b65e9fed631bf2a6716452f6ba9
The Mj <m.javad1391@gmail.com>
2023-06-25 11:08:34 +0330

    write GetAllSocialMedia useCase test & implement GetAllSocialMedia useCase method

--- internal/domains/socialMedia/usecase.go
+++ internal/domains/socialMedia/usecase.go
@@ -1,1 +1,36 @@
 package socialMedia
+
+import (
+	"Datearn/pkg/advancedError"
+	"Datearn/pkg/userHandler"
+	"context"
+)
+
+type SocialMediaUseCase struct {
+	sv        SocialMediaServiceInterface
+	decoderFn func(ctx context.Context, token string) (uint, error)
+}
+
+func NewSocialMediaUseCase(sv SocialMediaServiceInterface, decoderFn func(ctx context.Context, token string) (uint, error)) SocialMediaUseCaseInterface {
+	if decoderFn == nil {
+		decoderFn = userHandler.ExtractUserIdFromToken
+	}
+
+	return &SocialMediaUseCase{
+		sv:        sv,
+		decoderFn: decoderFn,
+	}
+}
+
+func (s *SocialMediaUseCase) GetAllSocialMedia(ctx context.Context, token string, request *SocialMediaGetRequest) (*[]SocialMedia, error) {
+	_, err := s.decoderFn(ctx, token)
+	if err != nil {
+		return nil, advancedError.New(err, "Decoding token failed")
+	}
+	return s.sv.GetAllSocialMedia(request.Id, request.Name, request.Limit, request.Offset)
+}
+
+func (s *SocialMediaUseCase) CreateSocialMedia(ctx context.Context, token string, request *SocialMediaCreateRequest) (*SocialMedia, error) {
+	//TODO implement me
+	panic("implement me")
+}
--- internal/domains/socialMedia/usecase_test.go
+++ internal/domains/socialMedia/usecase_test.go
@@ -1,1 +1,43 @@
 package socialMedia
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
+	"testing"
+)
+
+func TestSocialMediaUseCase_GetAllSocialMedia(t *testing.T) {
+	db, err := setupDbConnection()
+	assert.NoError(t, err, "Setup database connection failed")
+
+	ctx := context.Background()
+	randUserId := uint(1)
+	useCase := createSocialMediaUseCase(db, randUserId)
+
+	mockedSocialMedia := mockAndInsertSocialMedia(db, 1)
+
+	assert.Equal(t, len(mockedSocialMedia), 1, "Mocking products failed")
+
+	mockedGetSocialMediaRequest := mockGetSocialMediaRequest(&mockedSocialMedia[0].Id, &mockedSocialMedia[0].Name, nil, 0)
+
+	fetchedSocialMedia, err := useCase.GetAllSocialMedia(ctx, "", mockedGetSocialMediaRequest)
+	assert.NotNil(t, fetchedSocialMedia)
+	assertSocialMedia(t, mockedSocialMedia, *fetchedSocialMedia)
+
+}
+
+func createSocialMediaUseCase(db *gorm.DB, userId uint) SocialMediaUseCaseInterface {
+	return NewSocialMediaUseCase(NewSocialMediaService(NewSocialMediaRepository(db)), func(ctx context.Context, token string) (uint, error) {
+		return userId, nil
+	})
+}
+
+func mockGetSocialMediaRequest(id *uint, name *string, limit *int, offset int) *SocialMediaGetRequest {
+	return &SocialMediaGetRequest{
+		Id:     id,
+		Name:   name,
+		Limit:  limit,
+		Offset: offset,
+	}
+}
