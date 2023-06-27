a3f12da86770d0aa29390abddbffff9d4b119c3d
The Mj <m.javad1391@gmail.com>
2023-06-27 17:11:21 +0330

    write Like service test & implement Like service method

--- internal/domains/swipe/service.go
+++ internal/domains/swipe/service.go
@@ -10,27 +10,26 @@ func NewSwipeService(repo SwipeRepositoryInterface) SwipeServiceInterface {
 	}
 }
 
-func (s SwipeService) Like(likerId, likedId uint) (*Like, error) {
+func (s *SwipeService) Like(likerId, likedId uint) (*Like, error) {
+	return s.repo.Like(likerId, likedId)
+}
+
+func (s *SwipeService) DisableLike(likerId, likedId uint) (*Like, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (s SwipeService) DisableLike(likerId, likedId uint) (*Like, error) {
+func (s *SwipeService) DisLike(likerId, likedId uint) (*DisLike, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (s SwipeService) DisLike(likerId, likedId uint) (*DisLike, error) {
+func (s *SwipeService) DisableDisLike(likerId, likedId uint) (*DisLike, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (s SwipeService) DisableDisLike(likerId, likedId uint) (*DisLike, error) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (s SwipeService) GetAllLikes(likedId uint, limit *int, offset int) (*[]Like, error) {
+func (s *SwipeService) GetAllLikes(likedId uint, limit *int, offset int) (*[]Like, error) {
 	//TODO implement me
 	panic("implement me")
 }
--- internal/domains/swipe/service_test.go
+++ internal/domains/swipe/service_test.go
@@ -1,8 +1,25 @@
 package swipe
 
 import (
+	"github.com/stretchr/testify/assert"
 	"gorm.io/gorm"
+	"math/rand"
+	"testing"
 )
+
+func TestSwipeService_Like(t *testing.T) {
+	db, err := setupDbConnection()
+	assert.NoError(t, err, "Setup database connection failed")
+
+	sv := createSwipeService(db)
+	randLikerId := uint(rand.Int())
+	randLikedId := uint(rand.Int())
+	like := mockLike(randLikerId, randLikedId)
+
+	createdLike, err := sv.Like(like.LikerId, like.LikedId)
+	assert.NoError(t, err, "swipe service like creation failed")
+	assertLike(t, []Like{*like}, []Like{*createdLike})
+}
 
 func createSwipeService(db *gorm.DB) SwipeServiceInterface {
 	return NewSwipeService(NewSwipeRepository(db))
