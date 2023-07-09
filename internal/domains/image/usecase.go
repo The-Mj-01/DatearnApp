099fa571a1b5764c1a04455d7aa4f10d31107617
The Mj <m.javad1391@gmail.com>
2023-07-09 12:35:55 +0330

    write DeleteImage useCase test & implement DeleteImage UseCase method

--- internal/domains/image/usecase.go
+++ internal/domains/image/usecase.go
@@ -1,6 +1,7 @@
 package image
 
 import (
+	"Datearn/pkg/advancedError"
 	"Datearn/pkg/userHandler"
 	"context"
 )
@@ -23,22 +24,25 @@ func NewImageUseCase(sv ImageServiceInterface, decoderFn func(ctx context.Contex
 	}
 }
 
-func (i ImageUseCase) GetAllImage(ctx context.Context, token string, request *ImageGetRequest) (*[]Image, error) {
+func (i *ImageUseCase) GetAllImage(ctx context.Context, token string, request *ImageGetRequest) (*[]Image, error) {
+	_, err := i.decoderFn(ctx, token)
+	if err != nil {
+		return nil, advancedError.New(err, "Decoding token failed")
+	}
+	return i.sv.GetAllImage(request.Id, request.ImageableId, request.Name, request.ImageableType, request.Limit, request.Offset)
+}
+
+func (i *ImageUseCase) CreateImage(ctx context.Context, token string, request *ImageCreateRequest) (*Image, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i ImageUseCase) CreateImage(ctx context.Context, token string, request *ImageCreateRequest) (*Image, error) {
+func (i *ImageUseCase) UpdateImage(ctx context.Context, token string, request *ImageUpdateRequest) (*Image, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (i ImageUseCase) UpdateImage(ctx context.Context, token string, request *ImageUpdateRequest) (*Image, error) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (i ImageUseCase) DeleteImage(ctx context.Context, token string, request *ImageDeleteRequest) (*Image, error) {
+func (i *ImageUseCase) DeleteImage(ctx context.Context, token string, request *ImageDeleteRequest) (*Image, error) {
 	//TODO implement me
 	panic("implement me")
 }
--- internal/domains/image/usecase_test.go
+++ internal/domains/image/usecase_test.go
@@ -2,8 +2,35 @@ package image
 
 import (
 	"context"
+	"github.com/stretchr/testify/assert"
 	"gorm.io/gorm"
+	"math/rand"
+	"testing"
 )
+
+func TestImageUseCase_GetAllImage(t *testing.T) {
+	db, err := setupDbConnection()
+	assert.NoError(t, err, "Setup database connection failed")
+
+	ctx := context.Background()
+	randUserId := uint(1)
+	useCase := createImageUseCase(db, randUserId)
+
+	width := []int{200, 200, 200, 200, 200}
+	height := []int{200, 200, 200, 200, 200}
+	randImageableId := []uint{uint(rand.Int()), uint(rand.Int()), uint(rand.Int()), uint(rand.Int()), uint(rand.Int())}
+	randImageableType := []string{"Bio", "Bio", "Bio", "Bio", "Bio"}
+	randImageName := []string{"img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg"}
+
+	mockedImage := mockAndInsertImage(db, width, height, randImageableId, randImageableType, randImageName, 5)
+	assert.Equal(t, len(mockedImage), 5, "Mocking products failed")
+
+	mockedGetImageRequest := mockGetImageRequest(&mockedImage[0].Id, &mockedImage[0].ImageableId, &mockedImage[0].Name, &mockedImage[0].ImageableType, nil, 0)
+
+	fetchedImage, err := useCase.GetAllImage(ctx, "", mockedGetImageRequest)
+	assert.NotNil(t, fetchedImage)
+	assertImage(t, mockedImage, *fetchedImage)
+}
 
 func createImageUseCase(db *gorm.DB, userId uint) ImageUseCaseInterface {
 	return NewImageUseCase(NewImageService(NewImageRepository(db)), func(ctx context.Context, token string) (uint, error) {
