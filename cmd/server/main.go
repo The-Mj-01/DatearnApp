c18de2a8c8eff2c8cc471fb6a5fbe72a2f00311b
The Mj <m.javad1391@gmail.com>
2023-07-17 15:42:49 +0330

    Attach cmd server runner to http server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,37 @@
-package server
+package main
+
+import (
+	"Datearn/internal/config"
+	"Datearn/internal/server/http"
+	"Datearn/pkg/database"
+	"flag"
+	"fmt"
+	"log"
+)
+
+// migration defines migration flag
+var migration *bool = flag.Bool("migrate", false, "Run migrations")
+
+// main Where every shit starts
+func main() {
+	flag.Parse()
+	err := config.ReadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if *migration {
+		migrate()
+		return
+	}
+
+	http.StartHttpServer()
+}
+
+// migrate runs and executed migrations for sql structure
+func migrate() {
+	err := database.Migrate()
+	if err != nil {
+		fmt.Printf("%s: %s", "Running migrations faild with error", err)
+	}
+}
